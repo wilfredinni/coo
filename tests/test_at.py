d8629bc6d36c44dff70697f6a149748cb2a630ae
Carlos <carlos.w.montecinos@gmail.com>
2018-09-22 00:07:40 -0300

    Add Tests

---
+++ tests/test_at.py
@@ -0,0 +1,16 @@
+# from auto_tweet import AutoTweet
+import pytest
+from auto_tweet import delay_tweet, DELAY_STR
+
+
+@pytest.mark.parametrize(
+    "t_str, t_int",
+    [
+        ("half_hour", 2),  # 1800
+        ("one_hour", 4),  # 3600
+        ("tomorrow", 6),  # 86400
+        ("next_week", 8),  # 604800
+    ],
+)
+def test_delay_tweet(t_str, t_int):
+    assert delay_tweet(t_str, DELAY_STR) == t_int
