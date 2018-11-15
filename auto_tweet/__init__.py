3ebfa4d8862395357f9872fe0561185f8a098151
Carlos <carlos.w.montecinos@gmail.com>
2018-11-14 23:55:07 -0300

    AtoTweet to Coo

--- auto_tweet/__init__.py
+++
@@ -1,9 +0,0 @@
-from .auto_tweet import AutoTweet
-
-
-__title__ = "auto-tweet"
-__license__ = "Apache 2.0"
-__version__ = "0.1.0"
-__author__ = "Carlos Montecinos Geisse"
-__contact__ = "carlos.w.montecinos@gmail.com"
-__url__ = "https://github.com/wilfredinni/auto-tweet"
